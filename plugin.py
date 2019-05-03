9bc929bf88f68088c556095ddf3bf5b0343527d4
motsuka <marie@occupantfonts.com>
2019-05-03 13:31:43 -0400

    gitignore

--- plugin.py
+++ plugin.py
@@ -3,13 +3,15 @@
 __doc__="""
 Occupant QA Tool
 """
+import sys
+print(sys.version_info[0])
 
-from views.tasklistview import OCC_QATaskListView
+# from views.tasklistview import OCC_QATaskListView
 
 class OCC_QATool():
 	def __init__(self):
 		print "constructor called"
-		self.application = OCC_QATaskListView()
+		# self.application = OCC_QATaskListView()
 
 if __name__ == '__main__':
 	tool = OCC_QATool()
