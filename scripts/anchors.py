69789b1034eed4ed9134b51796150644471861c3
motsuka <marie@occupantfonts.com>
2019-06-12 11:28:28 -0400

    check narrow accents

--- scripts/anchors.py
+++ scripts/anchors.py
@@ -13,7 +13,7 @@ class Script(QATask):
 	def details(self):
 		return {
 			"name": "Top anchor alignment",
-			"version": "1.0.0",
+			"version": "1.3.0",
 			"description": "Checks the consistency of top anchors for floating accents"
 			}
 
@@ -35,7 +35,10 @@ class Script(QATask):
 		self.mark_categories = { 
 			"comb" : "Lowercase", 
 			"comb.case" : "Uppercase",
-			"comb.sc" : "Smallcaps"
+			"comb.sc" : "Smallcaps",
+			"comb.narrow" : "Lowercase",
+			"comb.narrow.case" : "Uppercase",
+			"comb.sc.narrow" : "Smallcaps"
 		}
 
 		non_floating = ["cedilla", "commaaccent", "ogonek", "caronvert", "horn"]
@@ -62,7 +65,9 @@ class Script(QATask):
 
 
 	def get_metrics(self, master, parameters):
-		"""Given reference glyph, stores the '_top' anchor points of the accent and its case (.case, .sc) counterparts for reference."""
+		"""Given reference accent, stores the '_top' anchor points of the accent and
+		its case (.case, .sc) counterparts for reference. If .case accents do not exist,
+		uses top accent position of A + reference accent"""
 		reference = { }
 		
 		# get reference glyphs
@@ -74,7 +79,8 @@ class Script(QATask):
 			# 		reference_mark = mark # i.e. macron, accent name
 
 			for extension in self.mark_categories:
-				mark_name = "%s%s" %(ref_accent_name, extension)
+				mark_name = "%s%s" %(ref_accent_name, extension) #i.e. macroncomb
+
 				if mark_name in self.glyphs:
 					# get _top anchor of mark
 					anchor = self.glyphs[mark_name].layers[master.id].anchors['_top']
@@ -83,16 +89,17 @@ class Script(QATask):
 					else:
 						self.report.note("* %s does not have a _top anchor" % mark_name)
 
+
 		else:
 			self.report.note("* Reference glyph does not exist in the font")
 
 		return reference
 
 
-	def check_anchors(self, master, points, report):
+	def check_anchors(self, master, all_marks, points, report):
 		for category in points:
 			# list of marks
-			marks = self.marks[category]
+			marks = all_marks[category]
 			ref_point = points[category]				
 
 			for mark in marks:
@@ -129,7 +136,6 @@ class Script(QATask):
 			for a in alignment_points:
 				report.note( "%s top anchors should be at %s" % (a, report.node(alignment_points[a]) ) )
 
-		
 			# check top anchor consistency within each mark category
-			self.check_anchors(master, alignment_points, report)
+			self.check_anchors(master, self.marks, alignment_points, report)
 
