193eb7bf4b7cf44e0a1835c79a76152878d7fee6
cem-morisawa <cem@morisawa-usa.com>
2019-06-24 15:39:32 -0400

    format unicode emojis

--- plugin.py
+++ plugin.py
@@ -6,7 +6,6 @@ Occupant QA Tool
 
 from GlyphsApp import *
 from vanilla import *
-from vanilla.test.testAll import Test
 import traceback
 
 from tasklistview import OCC_QATaskListView
@@ -15,12 +14,11 @@ class OCC_QATool():
 
 	def __init__(self):
 		Glyphs.showMacroWindow()
-		# Test()
 		if Glyphs.font:
-			print "\n\n🙌 Welcome to the Occupant QA Tool 🙌\n\n"
+			print u"\n\n🙌 Welcome to the Occupant QA Tool 🙌\n\n"
 			self.application = OCC_QATaskListView()
 		else:
-			print "There are no fonts open 😥 Please open a file and run the tool again."
+			print u"There are no fonts open 😥 Please open a file and run the tool again."
 
 if __name__ == '__main__':
 	tool = OCC_QATool()
